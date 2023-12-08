4acbd8b3d04ce201c8132e7ac85b2e8b1b6b97bb
bilalhanifkhatri <bilalhanifkhatri@gmail.com>
2023-12-09 00:02:11 +0500

    work on profile side, Logo Search, ProfileCard

---
+++ src/components/profileCard/ProfileCard.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import "./profileCard.css";
+import Cover from "../../img/cover.jpg";
+import Profile from "../../img/profileImg.jpg";
+
+const ProfileCard = () => {
+  return (
+    <div className="ProfileCard">
+      <div className="ProfileImages">
+        <img src={Cover} alt="" />
+        <img src={Profile} alt="" />
+      </div>
+      <div className="ProfileNames">
+        <span>Umair Satti</span>
+        <span>Senior UI/UX Designer</span>
+      </div>
+      <div className="followStatus">
+        <hr />
+        <div>
+          <div className="follow">
+            <span>6,122</span>
+            <span>followings</span>
+          </div>
+          <div className="vl"></div>
+          <div className="follow">
+            <span>1</span>
+            <span>followers</span>
+          </div>
+        </div>
+        <hr />
+      </div>
+      <span>My Profile</span>
+    </div>
+  );
+};
+
+export default ProfileCard;
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./home.css";
+import ProfileSide from "../../components/profileSide/ProfileSide";
+
+const Home = () => {
+  return (
+    <div className="Home">
+      <div className="profileSide">
+        <ProfileSide />
+      </div>
+      <div className="postSide">Post</div>
+      <div className="rightSide">Right</div>
+    </div>
+  );
+};
+
+export default Home;
